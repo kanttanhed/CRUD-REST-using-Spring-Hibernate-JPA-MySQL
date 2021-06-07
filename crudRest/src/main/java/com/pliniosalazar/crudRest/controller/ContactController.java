31f2b118cc6c1b8e1ee0297e47d212a3b3b92046
kanttanhed <plinio.salazar@yahoo.com.br>
2021-06-07 11:12:51 -0300

    Retrieve All Contacts and by ID (GET /contacts)

--- crudRest/src/main/java/com/pliniosalazar/crudRest/controller/ContactController.java
+++ crudRest/src/main/java/com/pliniosalazar/crudRest/controller/ContactController.java
@@ -1,6 +1,13 @@
 package com.pliniosalazar.crudRest.controller;
 
+import java.util.List;
+
+import com.pliniosalazar.crudRest.model.Contact;
 import com.pliniosalazar.crudRest.repository.ContactRepository;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +22,17 @@ public class ContactController {
     }
 
     // CRUD methods here
+    @GetMapping
+    public List findAll(){
+        return repository.findAll();
+    }
+
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity<Contact> findById(@PathVariable long id){
+    return repository.findById(id)
+          .map(record -> ResponseEntity.ok().body(record))
+          .orElse(ResponseEntity.notFound().build());
+}
+
+    
 }
