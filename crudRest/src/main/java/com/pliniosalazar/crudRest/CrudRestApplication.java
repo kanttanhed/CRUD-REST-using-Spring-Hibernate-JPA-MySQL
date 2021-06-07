c45a986f2b3032e8546017d60347f11a30ac16c0
kanttanhed <plinio.salazar@yahoo.com.br>
2021-06-07 10:06:30 -0300

    Initial setup

---
+++ crudRest/src/main/java/com/pliniosalazar/crudRest/CrudRestApplication.java
@@ -0,0 +1,13 @@
+package com.pliniosalazar.crudRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRestApplication.class, args);
+	}
+
+}
---
+++ crudRest/src/test/java/com/pliniosalazar/crudRest/CrudRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pliniosalazar.crudRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
